03aaa8082d0a82ddea00ea0e69240c85819a9cb2
Kai191911 <suvijack.b@gmail.com>
2024-10-08 14:02:35 +0700

    Delete ex00,ex01,ex02,ex03 directory

--- cell02/ex00/iszero.py
+++
@@ -1,5 +0,0 @@
-first_name = input("hey, what's your first name?: ")
-last_name = input("and your last name?: ")
-whole_name = first_name +" "+last_name
-
-print("well pleased to meet you", whole_name)
