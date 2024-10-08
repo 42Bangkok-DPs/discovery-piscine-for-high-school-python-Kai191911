e58237398f61e66f4b2277da15cb08acf869dfa4
Kai191911 <suvijack.b@gmail.com>
2024-10-08 12:07:23 +0700

    Create Whatyourname.py

---
+++ cell01/ex03/Whatyourname.py
@@ -0,0 +1,4 @@
+first_name = input("hey, what's your first name?: ")
+last_name = input("and your last name?: ") whole_name = first_name +" "+last_name
+
+print("well pleased to meet you", whole_name)
